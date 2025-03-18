ed4253fd99110064ca545fd65124e3ba605fb1e0
Rahul Singh <rahulx037@gmail.com>
2025-03-18 16:57:58 +0530

    reduxupdate

--- hooks/store.ts
+++ hooks/store.ts
@@ -1,5 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import todoReducer from './slices/todoSlice';
 import loginReducer from './slices/loginSlice';
 
